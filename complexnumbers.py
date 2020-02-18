35ea542aa4240a806d2b7836dc7eff23788aa403
Orion Sehn <orion.sehn@gmail.com>
2020-02-17 23:23:27 -0700

    finished the complex number module, and began work on modeling script for complex numbers

--- complexnumbers.py
+++ complexnumbers.py
@@ -11,19 +11,22 @@ i = sqrt(-1)
 cnum stores the info of a and b, and provides several operations for which
 complex numbers are defined. 
 
+
+getA()
+getB()
+getCoords()
 abs()
-__add__
-__sub__
-__mul__
-__truediv__
-__floordiv__
-__lt__
-__le__
-__gr__
-__ge__
-__ne__
-polar()
-compliment
+compliment()
+argument()
+__add__()
+__radd__()
+__sub__()
+__rsub__()
+__mul__()
+__rmul__()
+__truediv__()
+__rtruediv__()
+
 
 
 
@@ -38,69 +41,85 @@ class cnum():
     def __repr__(self):
         return f'{self.a} + {self.b}i'
 
+    def getA(self):
+        return self.a
+
+    def getB(self):
+        return self.b
+
+    def getCoords(self):
+        return (self.a, self.b)
+
     def compliment(self):
         return cnum(self.a, -1 * self.b)
 
-
     def argument(self):
         return np.arctan(self.b / self.a)
-
 
     def __abs__(self):
         return np.sqrt(self.a**2 + self.b**2)
 
     def __add__(self, other):
         if isinstance(other, cnum):
-            return cnum(self.a + other.a, self.b, other.b)
+            return cnum(self.a + other.a, self.b + other.b)
+        return cnum(self.a + other, self.b)
+
+    def __radd__(self, other):
+        if isinstance(other, cnum):
+            return cnum(self.a + other.a, self.b + other.b)
         return cnum(self.a + other, self.b)
 
     def __sub__(self, other):
         if isinstance(other, cnum):
-            return cnum(self.a - other.a, self.b, other.b)
+            return cnum(self.a - other.a, self.b - other.b)
         return cnum(self.a - other, self.b)
 
+    def __rsub__(self, other):
+        if isinstance(other, cnum):
+            return cnum(self.a - other.a, self.b - other.b)
+        return cnum(self.a - other, self.b)
 
     def __mul__(self, other):
         if isinstance(other, cnum):
             return cnum((self.a * other.a) - (self.b * other.b), 
             (self.a * other.b) + (self.b * other.a))
-        return cnum(other * self.a, other * self.b)
+        return cnum(self.a * other, self.b * other)
 
-    def __pow__(self, other):
-        if isinstance(other, int) != True:
-            AssertionError("exponent must be an integer value")
-        
+    def __rmul__(self, other):
+        if isinstance(other, cnum):
+            return cnum((self.a * other.a) - (self.b * other.b), 
+            (self.a * other.b) + (self.b * other.a))
+        return cnum(self.a * other, self.b * other)
 
     def __truediv__(self, other):
         if isinstance(other, cnum):
-            return ((abs(other) * abs(other)) * (self * other.compliment()))
-        return 
+            return cnum(((self.a * other.a) + (self.b * other.b))/ (other.a**2 + other.b**2),
+                         ((self.b * other.a) - (self.a * other.b))/(other.a**2 + other.b**2))
+        return self /cnum(other, 0)
     
-    def __floordiv__(self, other):
-        pass
-    def __lt__(self, other):
-        pass
-    def __le__(self, other):
-        pass
-    def __gr__(self, other):
-        pass
-    def __ge__(self, other):
-        pass
-    def __ne__(self, other):
-        pass
-    def polar():
-        pass
-
-
-
-testnum = cnum(2, 1)
-testnum2 = cnum(1, 2)
-print(f'{testnum}\n'
-f'Compliment: {testnum.compliment()}\n'
-f'Absolute value: {abs(testnum)} \n'
-f'Sqrt(5) = {np.sqrt(5)}\n'
-f'Argument: {testnum.argument()}\n'
-f'arctan(1/2) = {np.arctan(1/2)}')
-
-
+    def __rtruediv__(self, other):
+        if isinstance(other, cnum):
+            return cnum(((self.a * other.a) + (self.b * other.b))/ (other.a**2 + other.b**2),
+                         ((self.b * other.a) - (self.a * other.b))/(other.a**2 + other.b**2))
+        return self/cnum(other, 0)
+    
+    def test():
+        testnum = cnum(1, -3)
+        testnum2 = cnum(1, 2)
+        print(f'{testnum}\n'
+        f'Compliment: {testnum.compliment()}\n'
+        f'Absolute value: {abs(testnum)} \n'
+        #f'Sqrt(5) = {np.sqrt(5)}\n'
+        f'Argument: {testnum.argument()}\n'
+        #f'arctan(1/2) = {np.arctan(1/2)}'
+        f'Add 1 and 2: {testnum + testnum2}\n'
+        f'Add 1 and 2 Commutative: {5 + testnum2}\n'
+        f'Subtract 1 and 2: {testnum - testnum2}\n'
+        f'Subtract 1 and 2 commutative: {5 - testnum}\n'
+        f'Mulitpy 1 and 2: {testnum * testnum2}\n'
+        f'Mulitpy 1 and 2 Commutative: {5 * testnum2}\n' 
+        f'Divide 1 and 2: {testnum / testnum2}\n'
+        f'Divide 1 and 2 constant: {testnum / 10}\n'
+        f'Divide 1 and 2 commutative: {10 / testnum}\n')
+                                                                   
 
